4e97e8d2964703c1fcc88e381d0361b7563f0431
kotomi sasaki <jh23sk@gmail.com>
2024-08-05 22:20:44 +0900

    2220  Please enter the commit message for your changes. Lines starting

--- src/components/CardSkill.vue
+++ src/components/CardSkill.vue
@@ -6,21 +6,21 @@
       <template v-slot:title><span class="fw-bold">{{ cardType.title }}</span></template>
       <v-card-text>
         <p class="text">{{ cardType.explain }}</p>
-        <!-- <table class="table table-bordered m-0"> -->
-        <table class="table m-0">
+        <table class="table table-bordered m-0">
+        <!-- <table class="table m-0"> -->
         <!-- <v-table> --><!-- これ使うと、テーブルの下に横スクロールのバーが出る -->
           <thead class="table-secondary">
             <tr>
               <th scope="col" class="text-center" width="42%">技術</th>
               <th scope="col" class="text-center" width="3%"></th>
-              <th scope="col" class="text-center" width="25%">経験年数</th>
-              <th scope="col" class="text-center" width="30%">レベル</th>
+              <th scope="col" class="text-center" width="28%">経験年数</th>
+              <th scope="col" class="text-center" width="27%">レベル</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="(item, index) in cardType.items" :key="index">
-              <td width="42%">{{ item.tech }}</td>
-              <td width="3%">
+              <td>{{ item.tech }}</td>
+              <td>
                 <VTooltip v-if="item.note1" open-on-hover distance="8" class="v-tooltip" placement="top">
                   <span>※1</span>
                   <template #popper>{{ noteComment.note1 }}</template>
@@ -30,9 +30,9 @@
                   <template #popper>{{ noteComment.note2 }}</template>
                 </VTooltip>
               </td>
-              <td width="25%">{{ item.years }}</td>
-              <td width="30%">
-                <v-rating color="teal" large v-model=item.rating size="20" readonly></v-rating>
+              <td>{{ item.years }}</td>
+              <td>
+                <v-rating color="teal" large v-model=item.rating size="17" readonly></v-rating>
               </td>
             </tr>
           </tbody>
